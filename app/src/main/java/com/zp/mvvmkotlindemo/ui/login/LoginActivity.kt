1788a897e25bc3f59de2987a70f6161eaf4d1458
zhengpeng <306608923@qq.com>
2019-04-15 10:47:19 +0800

    项目初始化

---
+++ app/src/main/java/com/zp/mvvmkotlindemo/ui/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.zp.mvvmkotlindemo.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zp.mvvmkotlindemo.R
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
